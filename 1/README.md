7e9c3eb8e2932aa87365b44600f49d04523143e7
trmaxim92 <129587487+trmaxim92@users.noreply.github.com>
2024-08-09 00:00:27 +0400

    Delete 1 directory

--- 1/README.md
+++
@@ -1,1 +0,0 @@
-# YaTaxi_bot
--- 1/bottaxi/services/other_functions/phone_formatter.py
+++
@@ -1,6 +0,0 @@
-import re
-
-
-def phone_formatting(user_phone):
-    """Форматирование телеофона 79999999999."""
-    return int(re.sub(r'[+() -]', '', user_phone).replace(user_phone[0], '7', 1))
