0c9c60c6a34c39d1b6fd6838e731bb4434dc9769
trmaxim92 <129587487+trmaxim92@users.noreply.github.com>
2024-08-08 23:16:56 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,15 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
-from tgbot.config import load_config
-from tgbot.filters.admin import AdminFilter
-from tgbot.handlers.admin import register_admin
-from tgbot.handlers.errors import register_errors_handler
-from tgbot.handlers.user import register_user
-from tgbot.middlewares.database import DatabaseMiddleware
-from tgbot.middlewares.environment import EnvironmentMiddleware
-from tgbot.middlewares.throttling import ThrottlingMiddleware
-from tgbot.models.config import create_session_pool
+from bottaxi.config import load_config
+from bottaxi.filters.admin import AdminFilter
+from bottaxi.handlers.admin import register_admin
+from bottaxi.handlers.errors import register_errors_handler
+from bottaxi.handlers.user import register_user
+from bottaxi.middlewares.database import DatabaseMiddleware
+from bottaxi.middlewares.environment import EnvironmentMiddleware
+from bottaxi.middlewares.throttling import ThrottlingMiddleware
+from bottaxi.models.config import create_session_pool
 
 
 logger = logging.getLogger(__name__)
